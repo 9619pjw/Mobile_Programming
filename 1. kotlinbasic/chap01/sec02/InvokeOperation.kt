48d3f79d64c58032337747474b9e4d94b5e21751
JokerBear_96 <97871451+9619pjw@users.noreply.github.com>
2023-06-08 14:04:04 +0900

    Delete 1. kotlinbasic directory

--- 1. kotlinbasic/chap01/sec02/InvokeOperation.kt
+++
@@ -1,2 +0,0 @@
-package com.example.kotlinprojects.chap01.sec02
-
--- 1. kotlinbasic/chap05/sec01/AsExam.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kotlinprojects.chap05.sec01
-
-fun main(){
-    val userId = 1001
-    var idCheck: String?
-
-    idCheck = userId as? String?
-    println("idCheck : $idCheck")
-}
